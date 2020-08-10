923d2c29f6c6fa078a1fd23289f09f0694414d21
bijubasheer <bijubasheer@users.noreply.github.com>
2020-08-10 10:15:00 -0400

    Call Menu Service directly

--- src/extensions/jmfDdMenuExtApp/JmfDdMenuExtAppApplicationCustomizer.ts
+++ src/extensions/jmfDdMenuExtApp/JmfDdMenuExtAppApplicationCustomizer.ts
@@ -11,6 +11,11 @@ import styles from './AppCustomizer.module.scss';
 //import { escape } from '@microsoft/sp-lodash-subset';
 import * as strings from 'JmfDdMenuExtAppApplicationCustomizerStrings';
 import {AadHttpClient, HttpClient, IHttpClientOptions, HttpClientResponse } from '@microsoft/sp-http';
+import * as $ from 'jquery';
+
+require('myScript'); //Add a JS file
+declare function SayHello(msg): any; // Declare JS function
+declare function WireUpMenu():any;
 
 const LOG_SOURCE: string = 'JmfDdMenuExtAppApplicationCustomizer';
 
@@ -42,6 +47,8 @@ export default class JmfDdMenuExtAppApplicationCustomizer
   // render.
   this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
   
+  $("#spSiteHeader").hide();
+
   return Promise.resolve<void>();
   
   }
@@ -120,8 +127,8 @@ export default class JmfDdMenuExtAppApplicationCustomizer
   private CallAzureFunction(): Promise<any> {
 
     //const postURL = "https://menuserviceazfn.azurewebsites.net/api/GetMenu?code=uryuQaUpeJRt9RZbFava1nOOPQGmyfdEoch9gMf/o7CDrZ/USYuI5A==";
-    const postURL = "https://setmenuservice.azurewebsites.net/api/GetMenu?code=5ju70xjDJywCDUTI/xxxE/olwvdjZ4cRbzkiTWtwbxCrDi43Crg6jA==";
-    //const postURL = "https://set.dev.api.jmfamily.com/dd-gwmenusvc-sys/v1/api/menu";
+    //const postURL = "https://setmenuservice.azurewebsites.net/api/GetMenu?code=5ju70xjDJywCDUTI/xxxE/olwvdjZ4cRbzkiTWtwbxCrDi43Crg6jA==";
+    const postURL = "https://set.dev.api.jmfamily.com/dd-gwmenusvc-sys/v1/api/menu";
     //const body: string = '{"NameId":"conkqyg@JM","FirstName":"Biju","LastName":"Basheer","Spin":"","SETNumber":"09159","AppName":"VinSight","CallerName":"SharePoint","BrowserIE8": "False"}';
     const body: string = '{"NameId":"QAALLPAM@JM","FirstName":"QAALLPAM","LastName":"QAALLPAM","Spin":"","SETNumber":"09159","CallerName":"VinSight","BrowserIE8":"True"}';
     const requestHeaders: Headers = new Headers();
@@ -137,8 +144,8 @@ export default class JmfDdMenuExtAppApplicationCustomizer
     
     return this.context.aadHttpClientFactory
       //.getClient('89759aa2-8b1d-4e17-b0e3-56f9bcf71f71')
-      .getClient('54e0f8b3-971e-4f5d-90b0-083e29b433ac') //Azure Fn
-      //.getClient('1148b2ca-eded-4ea7-9f1e-4cce4bd47109') //Menu Service
+      //.getClient('54e0f8b3-971e-4f5d-90b0-083e29b433ac') //Azure Fn
+      .getClient('1148b2ca-eded-4ea7-9f1e-4cce4bd47109') //Menu Service
       .then((client: AadHttpClient): void => {
         client
           .post(postURL, AadHttpClient.configurations.v1, httpClientOptions)
@@ -146,37 +153,37 @@ export default class JmfDdMenuExtAppApplicationCustomizer
             return response.text();
           })
          .then(menuHTML => {  
-      //console.log(menuHTML);  
+      //console.log(menuHTML);
+      var oldLink = 'https://staging-admin-new.setdealerdaily.com:8082/';
+      var newLink = 'https://staging-admin.setdealerdaily.com/';
+      var cleanHTML = menuHTML.replace(oldLink, newLink);
+
       this._topPlaceholder.domElement.innerHTML = `
       <div class="no-index">
-        ${menuHTML}
+        ${cleanHTML}
       </div>`;
+
+      $('#TopLinksnavbarNav a').each(function(){
+        var updated_link = $(this).attr('href').replace('https://staging-admin-new.setdealerdaily.com:8082/', 'https://pubasfs.jmfamily.com/IdentityServer3/WSFederation?wa=wsignin1.0&authtype=azuread&wtrealm=urn%3adpamdinstgnew&wctx=ru=https://staging-admin.setdealerdaily.com/');
+        $(this).attr('href', updated_link);
+     });
+
+      //$('li.nav-item:contains("APPLICATIONS") a').attr("href", "https://staging-admin.setdealerdaily.com/applications/myapplications.aspx");
+      //$('li.nav-item:contains("REPORTS") a').attr("href", "https://staging-admin.setdealerdaily.com/report/myreports.aspx");
+      $('li.nav-item:contains("DEPARTMENTS") a').attr("href", "https://jmfamilystage.sharepoint.com/sites/DealerDaily/SitePages/Departments.aspx");
+      $('#menu-header a').attr("href", "https://jmfamilystage.sharepoint.com/sites/DealerDaily");
 
       let clickEvent= document.getElementById('DealershipnavbarDropdown');
       clickEvent.addEventListener("click", (e: Event) => this.ClickMenu(e));
 
+      //WireUpMenu();
+      //SayHello("Done loading menu");
+      
       return menuHTML;  
     }, (err: any): void => {
       // handle error here
       console.log(err );
     });
       });
-/*
-    return this.context.httpClient.post(postURL, HttpClient.configurations.v1, httpClientOptions)
-    .then((response: HttpClientResponse) => {  
-      return response.text();  
-    }) 
-    .then(menuHTML => {  
-      //console.log(menuHTML);  
-      this._topPlaceholder.domElement.innerHTML = `
-      <div class="no-index">
-        ${menuHTML}
-      </div>`;
-      return menuHTML;  
-    }, (err: any): void => {
-      // handle error here
-      console.log(err + "!");
-    });
-      */
   }
 }
